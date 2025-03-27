93c1c952e2605f6dd41a8d510467a1ab7a77cf3a
khavi <vib2105566@student.ctu.edu.vn>
2025-03-27 19:02:14 +0700

    Create BorrowList.vue

---
+++ src/components/BorrowList.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    props: {
+        borrows: { type: Array, default: [] },
+        activeIndex: { type: Number, default: -1 },
+    },
+    emits: ["update:activeIndex"],
+    methods: {
+        updateActiveIndex(index) {
+            this.$emit("update:activeIndex", index);
+        }
+    }
+};
+</script>
+
+<template>
+    <ul class="list-group">
+        <li
+            class="list-group-item"
+            v-for="(borrow, index) in borrows"
+            :key="borrow.MaSach + '-' + borrow.MaDocGia"
+            :class="{ active: index === activeIndex }"
+            @click="updateActiveIndex(index)"
+        >
+            <strong>Mã Độc Giả:</strong> {{ borrow.MaDocGia }} | 
+            <strong>Mã Sách:</strong> {{ borrow.MaSach }} | 
+            <strong>Ngày Mượn:</strong> {{ borrow.NgayMuon }} | 
+            <strong>Ngày Trả:</strong> {{ borrow.NgayTra }}
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.list-group-item.active {
+    background-color: #007bff;
+    color: white;
+    font-weight: bold;
+}
+</style>
