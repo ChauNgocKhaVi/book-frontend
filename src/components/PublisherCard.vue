811da73c520c59589abb2d43e99ac94ca25bf9d7
khavi <vib2105566@student.ctu.edu.vn>
2025-03-27 19:02:19 +0700

    Create PublisherCard.vue

---
+++ src/components/PublisherCard.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        props: {
+            publisher: { type: Object, required: true },
+        },
+    };
+</script>
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên nhà xuất bản:</strong>
+            {{ publisher.TenNXB }}
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ:</strong>
+            {{ publisher.DiaChi }}
+        </div>
+    </div>
+</template>
