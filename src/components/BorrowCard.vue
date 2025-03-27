696b2b2455c23a281deee24d8e0db30c528335e4
khavi <vib2105566@student.ctu.edu.vn>
2025-03-27 19:02:00 +0700

    Create BorrowCard.vue

---
+++ src/components/BorrowCard.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        props: {
+            borrow: { type: Object, required: true },
+        },
+    };
+</script>
+
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Mã Độc Giả:</strong>
+            {{ borrow.MaDocGia }}
+        </div>
+        <div class="p-1">
+            <strong>Mã Sách:</strong>
+            {{ borrow.MaSach }}
+        </div>
+        <div class="p-1">
+            <strong>Ngày Mượn:</strong>
+            {{ borrow.NgayMuon }}
+        </div>
+        <div class="p-1">
+            <strong>Ngày Trả:</strong>
+            {{ borrow.NgayTra }}
+        </div>
+    </div>
+</template>
